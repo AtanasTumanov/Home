73caeeebfbc07becb36a2afa1d33e4336b32f5a3
Атанас Туманов <atanastumanov@gmail.com>
2022-12-01 16:31:21 +0200

    Initial commit

---
+++ src/main/java/home/info/home/info.java
@@ -0,0 +1,13 @@
+package home.info.home;
+
+import java.util.Scanner;
+
+public class info {public static void main(String [] args){
+    System.out.println("Please enter your username:");
+    Scanner scanner = new Scanner(System.in);
+    String name = scanner.nextLine();
+    
+
+}
+
+}
