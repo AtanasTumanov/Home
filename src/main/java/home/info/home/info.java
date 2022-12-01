be98e2c60fd591ab475e921aac1dca86189df9db
Атанас Туманов <atanastumanov@gmail.com>
2022-12-01 16:31:21 +0200

    Initial commit

--- src/main/java/home/info/home/info.java
+++ src/main/java/home/info/home/info.java
@@ -2,12 +2,26 @@ package home.info.home;
 
 import java.util.Scanner;
 
-public class info {public static void main(String [] args){
-    System.out.println("Please enter your username:");
-    Scanner scanner = new Scanner(System.in);
-    String name = scanner.nextLine();
-    
+public class info {
+    public static void main(String[] args) {
+        System.out.println("Please enter your username:");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+        System.out.println("Please enter your hobby");
+        String hobby = scanner.nextLine();
 
-}
+        System.out.println("Hi," + username + " your hobby is " + hobby);
+        String txt = "reading";
 
-}
+        if (hobby=="reading");
+        System.out.println("They are 30 peaple with same hobby like you");
+
+
+    }
+
+    }
+
+
+
+
+
